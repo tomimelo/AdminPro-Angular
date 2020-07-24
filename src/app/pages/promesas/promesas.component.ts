04ca38040c284e1d5cb677ed159c4784f9199b2d
Tomas Melone <tomasmelone@hotmail.com>
2020-07-24 20:49:03 +0200

    Fin seccion 8

---
+++ src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { rejects } from 'assert';
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styleUrls: ['./promesas.component.css']
+})
+export class PromesasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.getUsuarios().then(usuarios => {
+      console.log(usuarios);
+    })
+
+    // const promesa = new Promise( (resolve, reject) => {
+    //   if(false) {
+    //     resolve("Hola Mundo");
+    //   } else {
+    //     reject('Algo salio mal');
+    //   }
+    // });
+
+    // promesa.then( (msg) => {
+    //   console.log(msg);
+    // })
+    // .catch(error => console.log("Error en mi promesa", error));
+
+    // console.log("Fin del init");
+  }
+
+  getUsuarios() {
+
+    return new Promise(resolve => {
+
+      fetch("https://reqres.in/api/users")
+      .then(resp => resp.json() )
+      .then(body => resolve(body.data));
+
+    });
+
+  }
+
+}
--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.css']
 })
-export class BreadcrumbComponent implements OnInit {
+export class BreadcrumbComponent implements OnDestroy{
 
-  constructor() { }
+  public titulo: string;
+  public tituloSubs$: Subscription;
 
-  ngOnInit(): void {
+  constructor( private router: Router) {
+
+    this.tituloSubs$ = this.getRouteArgs()
+                        .subscribe(({titulo}) => {
+                          this.titulo = titulo;
+                          document.title = `AdminPro - ${titulo}`;
+                        });
+                        
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getRouteArgs() {
+    return this.router.events
+      .pipe(
+        filter(event => event instanceof ActivationEnd),
+        filter( (event: ActivationEnd) => event.snapshot.firstChild === null),
+        map( (event: ActivationEnd) => event.snapshot.data)
+      )
   }
 
 }
